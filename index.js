13b27528d5c1784fd6bf87fdffa11136b4d0962b
Dammy <bestbynature2010@gmail.com>
2023-01-11 18:58:11 +0100

    Better Interface and more efficient algorithm

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 
-function playclick(letter){
-document.getElementById(letter).play();
-document.getElementById("display").innerText = `Button ${letter} has been clicked`
-}
+// function playclick(letter){
+// document.getElementById(letter).play();
+// document.getElementById("display").innerText = `Button ${letter} has been clicked`
+// }
 
 document.addEventListener('keypress', function(event){
-    let pressed = event.key;
-    pressed = pressed.toUpperCase()
+    let pressed = event.key, letters = "qweasdzxc";
+    pressed = pressed.toUpperCase();
+    if (!(letters.toUpperCase()).includes(pressed)) return
     document.getElementById(pressed).play();
     document.getElementById("display").innerText = `Key ${pressed} has been pressed`;
 })
 
+let buttons =  document.getElementsByClassName("drum-pad");
+
+buttons = Array.from(buttons).map(button=>{
+    button.addEventListener('click', (e)=>{
+        document.getElementById(e.target.innerText).play();
+        document.getElementById("display").innerText = `Button ${e.target.innerText} has been clicked`
+    })
+})
